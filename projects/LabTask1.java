09a1decbfca0269829b663295ea6f8febed1ea40
ANHT-TheBadChild <anht.thebadchild.11@gmail.com>
2026-09-18 00:02:58 +0800

    Refactor LabTask1 with added modulo methods

--- projects/LabTask1.java
+++ projects/LabTask1.java
@@ -2,22 +2,26 @@ import java.util.Scanner;
 
 public class LabTask1 {
     
-    public static int sum(int a, int b){
+    public static int addition(int a, int b){
         return a + b;
     }
     
-    public static int difference(int a, int b){
+    public static int substraction(int a, int b){
         return a - b;
     }
     
-    public static int product(int a, int b){
+    public static int multiplication(int a, int b){
         return a * b;
     }
     
-    public static double quotient(int a, int b){
+    public static double division(int a, int b){
         return (double)a / b;
     }
     
+    public static int modulo(int a, int b){
+        return a % b;
+    }
+
     public static void main(String[] args) {
         
         Scanner scan = new Scanner(System.in);
@@ -30,19 +34,20 @@ public class LabTask1 {
         System.out.printf("Please enter the second number: ");
         int b=scan.nextInt();
         
-        int sums = sum(a, b);
-        int diff = difference(a, b);
-        int prod = product(a, b);
-        double quo = quotient(a, b);
+        int sum = addition(a, b);
+        int difference = substraction(a, b);
+        int product = multiplication(a, b);
+        double quotient = division(a, b);
+        int remainder = modulo(a, b);
         
         System.out.println(" ");
         System.out.println("-----THE RESULTS-----");
         
-        System.out.println("Sum: " + sums);
-        System.out.println("Difference: " + diff);
-        System.out.println("Product: " + prod);
-        System.out.println("Quotient: " + quo);
-        
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.printf("Quotient: %.2f%n", quotient);
+        System.out.println("Remainder: " + remainder);
         scan.close();
     }    
 }
